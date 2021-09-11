d51650738f4da59a15e107a7ee99000ad34dcc6c
Mosolov Sergey <mosolovs1987@gmail.com>
2021-09-12 01:41:21 +0400

    added java bean to encapsulate todo entity

---
+++ src/main/java/com/ssau/laba1/model/TodoEntity.java
@@ -0,0 +1,71 @@
+package com.ssau.laba1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TodoEntity {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    private String body;
+
+    private String created;
+
+    private String deadline;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", body='" + body + '\'' +
+                ", created='" + created + '\'' +
+                ", deadline='" + deadline + '\'' +
+                '}';
+    }
+}
